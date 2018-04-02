b53814f00b89b24c6db2ae1d53bbd50ca4590176
nfc-ljc <lvjincheng@gyj.com>
2018-04-02 17:29:33 +0800

    左侧导航，根据传入不同参数，调用不同接口，生成导航栏

--- src/views/module/purchase.vue
+++ src/views/module/purchase.vue
@@ -2,26 +2,30 @@
 	<div>
 		<s3-breadcrumb :title="breadcrumb"></s3-breadcrumb>
 		<s3-leftMenu :leftNav="leftNav"></s3-leftMenu>
+		<router-view class="view"></router-view>
 	</div>
 </template>
 
 <script>
 
+
 import s3LeftMenu from '@/components/s3-leftMenu'
 import s3Breadcrumb from '@/components/s3-breadcrumb'
 
-	export default{
-		components:{
-			s3LeftMenu,
-			s3Breadcrumb
-		},
-		data(){
-			return {
-				breadcrumb:"采购订单",
-				leftNav:''
-			}
+export default{
+	components:{
+		s3LeftMenu,
+		s3Breadcrumb
+	},
+	data(){
+		return {
+			breadcrumb:"采购订单",
+			leftNav:'leftNav'
 		}
 	}
+}
+
+
 </script>
 
 <style scoped>
